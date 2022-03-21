0fa497061a7462d3fb8d7ca8ccf25d9a7862b5d1
Varga József Márk <vmark2145@gmail.com>
2022-03-21 17:53:08 +0100

    Changing personal credentials in pom.xml

---
+++ src/main/java/brickset/LegoSetRepository.java
@@ -0,0 +1,33 @@
+package brickset;
+
+import repository.Repository;
+
+/**
+ * Represents a repository of {@code LegoSet} objects.
+ */
+public class LegoSetRepository extends Repository<LegoSet> {
+
+    public LegoSetRepository() {
+        super(LegoSet.class, "brickset.json");
+    }
+
+    /**
+     * Returns the number of LEGO sets with the tag specified.
+     *
+     * @param tag a LEGO set tag
+     * @return the number of LEGO sets with the tag specified
+     */
+    public long countLegoSetsWithTag(String tag) {
+        return getAll().stream()
+                .filter(legoSet -> legoSet.getTags() != null && legoSet.getTags().contains(tag))
+                .count();
+    }
+
+
+    public static void main(String[] args) {
+        var repository = new LegoSetRepository();
+        System.out.println(repository.countLegoSetsWithTag("Microscale"));
+    }
+}
+
+}
